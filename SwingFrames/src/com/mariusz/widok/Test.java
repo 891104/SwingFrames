2df6ba6ceb03050dcd5465c08e9f7684b1c429d6
Mariusz <Mariusz@MariuszPC>
2016-10-24 17:10:18 +0200

    test

---
+++ SwingFrames/src/com/mariusz/widok/Test.java
@@ -0,0 +1,18 @@
+package com.mariusz.widok;
+
+import javax.swing.JFrame;
+
+public class Test {
+
+	public static void main(String[] args)
+	{
+		
+		SimpleFrame frame = new SimpleFrame();
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Test");
+		frame.setVisible(true);
+
+	}
+
+}
